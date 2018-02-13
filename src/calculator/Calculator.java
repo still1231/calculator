7ce8596b351e8a220680b4904d8f8aa821bd8818
still1231 <miha.b.7c@inbox.lv>
2018-02-13 20:28:26 +0200

    calculation operations + - / *

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import oracle.jrockit.jfr.StringConstantPool;
+
 /**
  * @author Dimitrijs Fedotovs <a href="http://www.bug.guru">www.bug.guru</a>
  * @version 1.0
@@ -8,7 +10,34 @@ package calculator;
 public class Calculator {
 
     public String calculate(String[] expression) {
-        return "0";
+        String a = expression[0];
+        String b = expression[2];
+        String op = expression[1];
+
+        double da = Double.parseDouble(a);
+        double db = Double.parseDouble(b); // 0.0
+
+        double result;
+        switch (op){
+            case "+": // mozhno dobavit' case "3": knopka dlja nazhatija
+            result = da + db;
+            break; // bez etogo srazu peredet k return "error";
+            case "-":
+                result = da - db;
+                break;
+            case "*":
+                result = da * db;
+                break;
+            case "/":
+                result = da / db;
+                break;
+            default:
+                return "Error"; // na slu4ai oshibki
+
+        }
+
+        return String.valueOf(result); // viklu4aet stroki
+
     }
 
 }
